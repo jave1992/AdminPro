b6dcee734a96f0b105bceccc2896dc2192e9aa2a
Jair Vasquez <jair1624@gmail.com>
2020-06-14 14:41:24 -0500

    Actualizacion a Heroku

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/adminpro'));
+app.use(express.static(__dirname + '/dist/adminpro'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/adminpro/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+    console.log(PORT);
+});
